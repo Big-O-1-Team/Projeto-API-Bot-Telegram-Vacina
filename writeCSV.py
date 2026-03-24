8d51e019f7cdc53c73aed7c2911746ece6ea7840
Lennon Vinicius de Moraes Soares <lennonmoraes.dev@gmail.com>
2026-03-24 16:07:52 -0300

    Função para criar tabela CSV

---
+++ writeCSV.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import csv
+tabelaCSV = {"Categoria":[],
+             "Informação":[]
+             }
+with open('categoriaInformaçoes.csv', 'r') as arquivo:
+    arquivo_csv =csv.reader(arquivo)
+
+df = pd.DataFrame(tabelaCSV)
+def newLine(vetor, tabela):
+    for dado in range(len(vetor)):
+        tabela.loc[len(tabela)] = vetor[dado]
+    print(tabela)
+    return tabela
+
+def mostrarTabele(tabela):
+    print(tabela)
