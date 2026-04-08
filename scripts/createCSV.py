e0038f1d18c8ba40df69d86ae96c619ab28d46c6
Lennon Vinicius de Moraes Soares <lennonmoraes.dev@gmail.com>
2026-04-08 10:03:24 -0300

    fix: add utf-8
    o csv salva os dados no formato utf-8

--- scripts/createCSV.py
+++ scripts/createCSV.py
@@ -2,14 +2,15 @@ import pandas as pd
 from IPython.display import display
 
 def CriarCSV(tabela):
-    df = pd.DataFrame(tabela, columns=["Categoria","periodo", "Vacina", "Doenças Evitadas"])
+    df = pd.DataFrame(tabela, columns=["Categoria","periodo", "Vacina", "Doencas Evitadas"])
     CSVFile = df.to_csv("CategoriaInformacoes.csv", index=False)
     display(df)
     return CSVFile
 
 def procuraInfoPCategoria(categoria):
-    with open("CategoriaInformacoes.csv", 'r') as arquivo:
+    with open("CategoriaInformacoes.csv", 'r', encoding = 'utf-8') as arquivo:
         CSVFile= pd.read_csv(arquivo)
         infoFiltrada = CSVFile[CSVFile['Categoria'] == categoria]
+        infoFiltrada.columns = infoFiltrada.columns.str.strip()
 
-    return infoFiltrada[['periodo', 'Vacina', 'Doenças Evitadas']].values.tolist()
+    return infoFiltrada[['periodo', 'Vacina', 'Doencas Evitadas']].values.tolist()
